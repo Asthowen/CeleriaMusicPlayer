42db44d5a8db1e49b4722831c18e0f1657ae6a50
Sdds <sddslebg@gmail.com>
2020-08-29 17:58:25 +0200

    First commit

--- src/main/java/fr/musicplayer/celeria/Main.java
+++ src/main/java/fr/musicplayer/celeria/Main.java
@@ -1,4 +1,13 @@
 package fr.musicplayer.celeria;
 
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            Class.forName("javafx.application.Application");
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null,"");
+        }
+    }
 }
