43246ca048760023f35e0c89757aee136e2aae8b
Raphaël <59535754+Asthowen@users.noreply.github.com>
2020-09-13 10:51:42 +0200

    Fix the fucking shit I made with the Logger

--- src/main/java/fr/musicplayer/celeria/Main.java
+++ src/main/java/fr/musicplayer/celeria/Main.java
@@ -5,10 +5,18 @@ import fr.musicplayer.celeria.utils.Logger;
 import javafx.application.Application;
 
 import javax.swing.*;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Main {
     public static Logger logger = new Logger();
+
     public static void main(String[] args) {
+        try {
+            PrintWriter w = new PrintWriter(logger.getHomeDirectory() + "/Logs.log");
+            w.print("");
+            w.close();
+        } catch (FileNotFoundException e) {}
 
         try {
             Class.forName("javafx.application.Application");
@@ -16,7 +24,6 @@ public class Main {
             logger.error("JavaFx not installed on your computer.");
             JOptionPane.showMessageDialog(null,"You must have Javafx installed on your computer", "Error" , 0);
         }
-
         Application.launch(FxApplication.class, args);
     }
 }
